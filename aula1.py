289b30cbe59502f1fd341d809b0b023ad6178a1b
cl-daniel <carlosdff.11@gmail.com>
2024-09-11 14:19:56 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,20 @@
+# Aqui eu posso adicionar comentários
+# Continuar na linha de baixo
+print("Daniel") # comentário a linha do código
+print('Daniel') 
+print('Daniel "Danzin" Ferreira')
+print("Daniel 'Danzin' Ferreira")
+print("Daniel\"Danzin\" Ferreira")
+      
+teste = "oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(teste(teste))
+teste = 12,1
+print(teste)
+print(teste(teste))
+teste = False
+print(teste)
+print(teste(teste))
---
+++ aula2.py
@@ -0,0 +1,9 @@
+# Atribuição de variáveis
+Nome = "Daniel"
+Idade = 30
+Altura = 1.83
+
+# exibir mensagem na tela
+print("Nome: ", Nome, "Idade: ", Idade "Altura: ", Altura)
+# formatação com f-string
+print(f"Nome: {Nome} Idade: {Idade} Altura: {Altura}")
