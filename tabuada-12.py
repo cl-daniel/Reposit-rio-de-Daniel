f90122f332c4c15796625f56fa2c0318d1d78f87
cl-daniel <carlosdff.11@gmail.com>
2024-09-26 18:17:18 -0300

    Add files via upload

---
+++ tabuada-12.py
@@ -0,0 +1,7 @@
+
+for multiplicador in range (1,11):
+    for contador in range (1,11):
+        resultado = multiplicador * contador
+        print(f"{multiplicador} X {contador} = {resultado}")
+        print()
+    
